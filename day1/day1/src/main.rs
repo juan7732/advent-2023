3ccdb545abdd3a3d68271a52be481ecdcda203fc
Juan Ruiz Valencia <juan7732@gmail.com>
2025-04-05 17:27:48 -0500

    backing up

--- day1/day1/src/main.rs
+++ day1/day1/src/main.rs
@@ -1,13 +1,20 @@
-pub mod parts {
-    pub fn part_1() {
-        println!("part 1");
-    }
-    pub fn part_2() {
-        println!("part 2");
+use std::fs::File;
+use std::io::{self, Read};
+
+fn main() {
+    let file_path = "./src/data/test.txt";
+    match load_data(file_path) {
+        Ok(()) => println!("File read successfully"),
+        Err(e) => println!("Error reading file: {:?}", e),
     }
 }
 
-fn main() {
-    parts::part_1();
-    parts::part_2();
+fn load_data(file_path: &str) -> io::Result<()> {
+    let mut file = File::open(file_path)?;
+    let mut contents = String::new();
+    file.read_to_string(&mut contents)?;
+    println!("in file {}", file_path);
+    println!("With text:\n{contents}");
+    Ok(())
 }
+
